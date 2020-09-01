1a8c0e53dd3e9153ea971514bfe3721238cc7fae
Divyam Soni <divyam.8116@gmail.com>
2020-09-01 15:49:12 +0530

    added GPTM_compare

---
+++ Bare-metal/7-GPTM_compare/Objects/system_stm32f7xx.d
@@ -0,0 +1,10 @@
+.\objects\system_stm32f7xx.o: RTE\Device\STM32F767ZITx\system_stm32f7xx.c
+.\objects\system_stm32f7xx.o: D:\Softwares\Arm\Packs\Keil\STM32F7xx_DFP\2.11.0\Drivers\CMSIS\Device\ST\STM32F7xx\Include\stm32f7xx.h
+.\objects\system_stm32f7xx.o: D:\Softwares\Arm\Packs\Keil\STM32F7xx_DFP\2.11.0\Drivers\CMSIS\Device\ST\STM32F7xx\Include\stm32f767xx.h
+.\objects\system_stm32f7xx.o: D:\Softwares\Arm\Packs\ARM\CMSIS\5.5.1\CMSIS\Core\Include\core_cm7.h
+.\objects\system_stm32f7xx.o: D:\Softwares\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\system_stm32f7xx.o: D:\Softwares\Arm\Packs\ARM\CMSIS\5.5.1\CMSIS\Core\Include\cmsis_version.h
+.\objects\system_stm32f7xx.o: D:\Softwares\Arm\Packs\ARM\CMSIS\5.5.1\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\system_stm32f7xx.o: D:\Softwares\Arm\Packs\ARM\CMSIS\5.5.1\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\system_stm32f7xx.o: D:\Softwares\Arm\Packs\ARM\CMSIS\5.5.1\CMSIS\Core\Include\mpu_armv7.h
+.\objects\system_stm32f7xx.o: D:\Softwares\Arm\Packs\Keil\STM32F7xx_DFP\2.11.0\Drivers\CMSIS\Device\ST\STM32F7xx\Include\system_stm32f7xx.h
---
+++ Bare-metal/7-GPTM_compare/main.c
@@ -0,0 +1,27 @@
+#include "stm32f7xx.h"                  // Device header
+
+/*
+
+Toggle the LED using TIM2
+AF2 <-> TIM3_CH3N <-> PB0 <-> LED1 (GREEN)
+
+*/
+
+int main(){	
+	RCC->AHB1ENR |= 2;										// Enable clock to GPIOB
+	GPIOB->MODER |= 2;										// Set PB0 to Alternate Function
+	GPIOB->AFR[0] |= 2;										// Set pin AF2 for AFR0
+
+	RCC->APB1ENR |= 2;										// Enable clock for TIM3
+	TIM3->PSC = 1600-1;										// Divide 16,000,000 by 	1,600
+	TIM3->ARR = 10000-1;									// Divide 		10,000 by 	10,000
+	TIM3->CNT = 0;												// Clear the TIM3 counter
+	TIM3->CR1 = 1;												// Enable the TIM3 counter
+	TIM3->CCMR2 = 0x30;										// Output compare 3 mode
+	TIM3->CCR1 = 0;												// Set match mode
+	TIM3->CCER = 0x100;										// CH3 channel configured as input
+	TIM3->CNT = 0;												// Clear counter
+	TIM3->CR1 = 1;												// Enable TIM3
+	
+	while(1){}
+}
