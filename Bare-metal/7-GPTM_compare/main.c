fd704714286624d14e5ad1ff834d3b8a7ddf8679
Divyam <divyam.8116@gmail.com>
2020-09-01 15:50:32 +0530

    Update main.c

--- Bare-metal/7-GPTM_compare/main.c
+++ Bare-metal/7-GPTM_compare/main.c
@@ -13,15 +13,15 @@ int main(){
 	GPIOB->AFR[0] |= 2;										// Set pin AF2 for AFR0
 
 	RCC->APB1ENR |= 2;										// Enable clock for TIM3
-	TIM3->PSC = 1600-1;										// Divide 16,000,000 by 	1,600
-	TIM3->ARR = 10000-1;									// Divide 		10,000 by 	10,000
-	TIM3->CNT = 0;												// Clear the TIM3 counter
-	TIM3->CR1 = 1;												// Enable the TIM3 counter
+	TIM3->PSC = 1600-1;										// Divide 16,000,000 by 1,600
+	TIM3->ARR = 10000-1;										// Divide 10,000 by 10,000
+	TIM3->CNT = 0;											// Clear the TIM3 counter
+	TIM3->CR1 = 1;											// Enable the TIM3 counter
 	TIM3->CCMR2 = 0x30;										// Output compare 3 mode
-	TIM3->CCR1 = 0;												// Set match mode
+	TIM3->CCR1 = 0;											// Set match mode
 	TIM3->CCER = 0x100;										// CH3 channel configured as input
-	TIM3->CNT = 0;												// Clear counter
-	TIM3->CR1 = 1;												// Enable TIM3
+	TIM3->CNT = 0;											// Clear counter
+	TIM3->CR1 = 1;											// Enable TIM3
 	
 	while(1){}
 }
