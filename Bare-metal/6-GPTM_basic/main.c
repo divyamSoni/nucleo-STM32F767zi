9965ff0b65bdfae7df85b883e5a8991b84f26330
Divyam Soni <divyam.8116@gmail.com>
2020-09-01 15:48:26 +0530

    modified GPTM_basic

--- Bare-metal/6-GPTM_basic/main.c
+++ Bare-metal/6-GPTM_basic/main.c
@@ -2,7 +2,7 @@
 
 int main(){	
 	RCC->AHB1ENR |= 0x2;		// Enable clock for GPIOB
-	GPIOB->MODER |= 0x4000;	// General Purpose output mode for GPIOB_MODER7
+	GPIOB->MODER |= 0x4000;		// General Purpose output mode for GPIOB_MODER7
 	
 	/* 	Timer config
 			1 Hz TIMER
@@ -10,8 +10,8 @@ int main(){
 	RCC->APB1ENR |= 1;			// Enable clock for Timer2
 	TIM2->PSC = 1600-1;			// Divide 16,000,000 by 1600
 	TIM2->ARR = 10000-1;		// Divide 10,000 by 10,000
-	TIM2->CNT = 0;					// Clear the Timer2 counter
-	TIM2->CR1 = 1;					// Enable the Timer2 counter
+	TIM2->CNT = 0;				// Clear the Timer2 counter
+	TIM2->CR1 = 1;				// Enable the Timer2 counter
 	
 	while(1){
 		while(!(TIM2->SR & 1)){}
