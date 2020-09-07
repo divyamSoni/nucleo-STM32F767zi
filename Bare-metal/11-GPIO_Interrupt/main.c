0d86c1816ff0d2e69f43e8074fa102548d1cbaba
Divyam <divyam.8116@gmail.com>
2020-09-07 20:16:04 +0530

    Update main.c

--- Bare-metal/11-GPIO_Interrupt/main.c
+++ Bare-metal/11-GPIO_Interrupt/main.c
@@ -7,7 +7,7 @@ void EXTI15_10_IRQHandler(void);
 
 int main(void){
 	
-	__disable_irq();						// Disable global interrupt
+	__disable_irq();			// Disable global interrupt
 	
 	RCC->AHB1ENR |=  0x2; 			// Enable clock access to GPIOB
 	RCC->AHB1ENR |=  0x4; 			// Enable clock access to GPIOC
@@ -16,18 +16,18 @@ int main(void){
 				
 	// Setting Output mode for LEDs
 	
-	GPIOB->MODER |= 0x1;							// LD1 <-> PB0	
-	GPIOB->MODER |= 0x4000;						// LD2 <-> PB7
+	GPIOB->MODER |= 0x1;				// LD1 <-> PB0	
+	GPIOB->MODER |= 0x4000;				// LD2 <-> PB7
 	GPIOB->MODER |= 0x010000000;			// LD3 <-> PB14
 	// By default PC13 is input (00)
 	
-	SYSCFG->EXTICR[3] |= 0x0020;	// Select Port C for EXTI13
-	EXTI->IMR |= 0x2000;					// Unmask EXTI13
-	EXTI->FTSR |= 0x2000;					// Select falling edge trigger
+	SYSCFG->EXTICR[3] |= 0x0020;			// Select Port C for EXTI13
+	EXTI->IMR |= 0x2000;				// Unmask EXTI13
+	EXTI->FTSR |= 0x2000;				// Select falling edge trigger
 	
 	NVIC_EnableIRQ(EXTI15_10_IRQn);
 	
-	__enable_irq();							// Enable global interrupt
+	__enable_irq();					// Enable global interrupt
 	
 	while(1){}	// doing something here
 }
@@ -44,20 +44,18 @@ void EXTI15_10_IRQHandler(void){
 	GPIOB->ODR = 0;
 	sysTickDelayMs(400);
 	
-	EXTI->PR = 0x2000;					// Clear the interrupt flag
+	EXTI->PR = 0x2000;				// Clear the interrupt flag
 }
 
 void sysTickDelayMs(int n){
 	// Clock 16 MHz
 	SysTick->LOAD = 16000; 		// Load with number of clocks per milisecond
-	SysTick->VAL = 0;					// Clear current value register
-	SysTick->CTRL = 0x5;			// Enable SysTick
+	SysTick->VAL = 0;		// Clear current value register
+	SysTick->CTRL = 0x5;		// Enable SysTick
 	
 	for(int i = 0; i<n; i++){	
 		// Wait until the count flag is set
 		while((SysTick->CTRL & 0x10000) == 0){}
-	
 	}
-	
 	SysTick->CTRL = 0;
 }	
