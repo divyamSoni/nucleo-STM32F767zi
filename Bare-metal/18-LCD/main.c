e0dffdc84a030707a6ddccb6ff0e782b50b6668e
Divyam <divyam.8116@gmail.com>
2020-10-15 13:57:34 +0530

    Update main.c

--- Bare-metal/18-LCD/main.c
+++ Bare-metal/18-LCD/main.c
@@ -127,10 +127,10 @@ void lcd_init(void){
 
 void GPIOInit(void){
 	
-	RCC->AHB1ENR |= 0x0A;											// Enable clock access to GPIO B,D
+	RCC->AHB1ENR |= 0x0A;				// Enable clock access to GPIO B,D
 	GPIOB->MODER |= (1<<30)|(1<<26)|(1<<24); 	// Set PB12,13,15 as output
-	GPIOB->BSRR 	= (EN|RW)<<16;							// Set EN and RW low (PB12, PB13)
-	GPIOD->MODER |= 0x5555;										// Set PD0 - PD7 as output
+	GPIOB->BSRR 	= (EN|RW)<<16;			// Set EN and RW low (PB12, PB13)
+	GPIOD->MODER |= 0x5555;				// Set PD0 - PD7 as output
 
 }
 
@@ -138,19 +138,19 @@ void lcd_command(unsigned char command){
 
 	GPIOB->BSRR = (RS|RW)<<16;			// RS = RW = 0
 	GPIOD->ODR = command;
-	GPIOB->BSRR = EN;								// E = 1
+	GPIOB->BSRR = EN;				// E = 1
 	sysTickDelayMs(50);
-	GPIOB->BSRR = EN<<16;						// E = 0
+	GPIOB->BSRR = EN<<16;				// E = 0
 
 }
 
 void lcd_data(char data){
 	
-	GPIOB->BSRR = RS;							// RS = 1
+	GPIOB->BSRR = RS;					// RS = 1
 	GPIOB->BSRR = RW<<16;					// RW = 0
-	GPIOD->ODR = data;						// Pass data
-	GPIOB->BSRR = EN;							// EN = 1
-	sysTickDelayMs(30);						// delay
+	GPIOD->ODR = data;					// Pass data
+	GPIOB->BSRR = EN;					// EN = 1
+	sysTickDelayMs(30);
 	GPIOB->BSRR = EN<<16;					// EN = 0
 
 }
@@ -158,8 +158,8 @@ void lcd_data(char data){
 void sysTickDelayMs(int n){
 	// Clock 16 MHz
 	SysTick->LOAD = 16000; 		// Load with number of clocks per milisecond
-	SysTick->VAL = 0;					// Clear current value register
-	SysTick->CTRL = 0x5;			// Enable SysTick
+	SysTick->VAL = 0;		// Clear current value register
+	SysTick->CTRL = 0x5;		// Enable SysTick
 
 	for(int i = 0; i<n; i++){
 		// Wait until the count flag is set
