f206cac0ad3c843f2676a822117db32e2195392f
Divyam <divyam.8116@gmail.com>
2020-11-08 23:01:49 +0530

    Update main.c

--- Bare-metal/22-SPI_basic/main.c
+++ Bare-metal/22-SPI_basic/main.c
@@ -12,28 +12,28 @@ void SPI_Init(void);
 void SPI_write_data(unsigned char data);
 
 int main(void){
-	
+	// SPI device not connected in this example
 }
 
 void SPI_Init(void){
 	
 	// Initialize the GPIO
-	RCC->AHB1ENR |= 1UL;													// GPIOA clock access
-	RCC->APB2ENR |= 1<<12;												// SPI1 clock access
+	RCC->AHB1ENR |= 1UL;					// GPIOA clock access
+	RCC->APB2ENR |= 1<<12;					// SPI1 clock access
 	
 	GPIOA->MODER &= ~(unsigned int)0x0000FF00;		// Clear PA-5, 7
-	GPIOA->MODER |= 0x00008800;										// Alternate function PA-5, 7
+	GPIOA->MODER |= 0x00008800;				// Alternate function PA-5, 7
 	
 	GPIOA->AFR[0] &= ~(unsigned int)0xF0F00000;		// Clear PA-5, 7
-	GPIOA->AFR[0] |= 0x50500000;									// AF5 PA-5, 7
+	GPIOA->AFR[0] |= 0x50500000;				// AF5 PA-5, 7
 	
 	GPIOA->MODER &= ~(unsigned int)0x00000300;		// Clear PA-4
-	GPIOA->MODER |= 0x00000100;										// PA-4 as output (SS)
+	GPIOA->MODER |= 0x00000100;				// PA-4 as output (SS)
 	
 	// Initailize SPI
-	SPI1->CR1 |= (1<<11)|(1<<4)|(1<<3);						// 8 bit data mode, baud rate
+	SPI1->CR1 |= (1<<11)|(1<<4)|(1<<3);			// 8 bit data mode, baud rate
 	SPI1->CR2 = 0;
-	SPI1->CR1 |= (1<<6);													// SPI enable
+	SPI1->CR1 |= (1<<6);					// SPI enable
 }
 
 void SPI_write_data(unsigned char data){
