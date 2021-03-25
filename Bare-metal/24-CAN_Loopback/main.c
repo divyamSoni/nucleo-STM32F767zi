2e210d04056e5f840f037d6d02ab514bfbfb4026
Divyam Soni <divyam.8116@gmail.com>
2021-03-25 19:22:52 +0530

    added fn can1_msg_info

--- Bare-metal/24-CAN_Loopback/main.c
+++ Bare-metal/24-CAN_Loopback/main.c
@@ -1,5 +1,5 @@
 #include "can.h"
-#include "stm32f7xx.h"                  // Device header
+#include "stm32f7xx.h"      // Device header
 
 /*	Configure the CAN in loopback mode
  *	with mailbox 0, when message is
@@ -9,30 +9,23 @@
 
 int main(){
 	// Message info to be transmitted
-	CAN_TxMsg.id = 0x15A;
-	char* string = "_canBus_";	// 8 char data to be sent
-	for(int i=0;i<8;i++){
-		CAN_TxMsg.data[i] = string[i];
-	}
-	CAN_TxMsg.len = 8;
-	CAN_TxMsg.format = 0;
-	CAN_TxMsg.type = 0;
-
+	CAN1_Tx_Info(0x15A,"_canBus_",8,0,0);
 	CAN1_ready();
 	GPIOA_Init();
 	CAN1_Init();
 	Interrupt_Init();
 
 	while(1){
-		if(CAN1_TxRdy){
-			CAN1_TxRdy = 0;
-			CAN1_Tx(&CAN_TxMsg);
+        if(CAN1_TxRdy){
+            CAN1_TxRdy = 0;
+            CAN1_Tx(&CAN_TxMsg);
         }
-		if(CAN1_RxRdy){
-	       CAN1_RxRdy = 0;
-           CAN1_Rx(&CAN_RxMsg);
+		sysTickDelayMs(500);
+        if(CAN1_RxRdy){
+            CAN1_RxRdy = 0;
+            CAN1_Rx(&CAN_RxMsg);
         }
-        for(int i = 0; i<8 ; i++){
+		for(int i = 0; i<8 ; i++){
             if(CAN_TxMsg.data[i] == CAN_RxMsg.data[i]){
                 LED_toggle(0x80); // BLUE LED
 			}
@@ -42,12 +35,12 @@ int main(){
 
 void GPIOA_Init(void){
 	/*	PA11 - CAN1_Rx - AF9
-		PA12 - CAN1_Tx - AF9	*/
+		PA12 - CAN1_Tx - AF9  */
 	RCC->AHB1ENR |= 3;             // Enable clock access to GPIOA,B
 
-	GPIOB->MODER |= 0x1;           // LD1 <-> PB0  <-> Red		<-> 0x4000
-	GPIOB->MODER |= 0x4000;        // LD2 <-> PB7  <-> Blue	<-> 0x0080
-	GPIOB->MODER |= 0x010000000;   // LD3 <-> PB14 <-> Green	<-> 0x0001
+	GPIOB->MODER |= 0x1;           // LD1 <-> PB0  <-> Red     <-> 0x4000
+	GPIOB->MODER |= 0x4000;        // LD2 <-> PB7  <-> Blue	   <-> 0x0080
+	GPIOB->MODER |= 0x010000000;   // LD3 <-> PB14 <-> Green   <-> 0x0001
 
 	GPIOA->AFR[1] |= 0x99000;				// Enabling CAN1 on PA11, PA12
 	GPIOA->MODER |= (1UL<<23)|(1UL<<25);	// PA11, PA12 <-> Alternate Function
