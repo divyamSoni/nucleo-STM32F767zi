7127316946fa0ada83ac499036822fdca84f83d6
Divyam Soni <divyam.8116@gmail.com>
2021-05-10 09:10:15 +0530

    updated blinky

--- Bare-metal/01-blinky/main.c
+++ Bare-metal/01-blinky/main.c
@@ -1,9 +1,9 @@
 /***************************
-** Use a loop delay to		**
-** blink an LED						**
-** 1. LED2(Blue) <-> PB7	**
-** 2. AHB1 <-> GPIOB			**
-** 3. RCC->AHB1ENR				**
+** Use a loop delay to	  **
+** blink an LED			  **
+** 1. LED2(Blue) <-> PB7  **
+** 2. AHB1 <-> GPIOB	  **
+** 3. RCC->AHB1ENR        **
 ***************************/
 
 #include "stm32f7xx.h"			// Device header
@@ -12,15 +12,15 @@ void delayMs(int delay);
 
 int main(void)
 {
-	RCC->AHB1ENR |= 0x2;			// Enable clock access to GPIOB
+	RCC->AHB1ENR |= 0x2;		// Enable clock access to GPIOB
 	GPIOB->MODER |= 0x4000;		// GPIOB_PIN_7 output mode
 
 	while(1)
 	{
-		GPIOB->ODR |= 1UL << 7;			// LED2 ON
-		delayMs(1000);							// Loop delay
+		GPIOB->ODR |= 1UL << 7;		// LED2 ON
+		delayMs(1000);				// Loop delay
 		GPIOB->ODR &= ~(1UL << 7);	// LED2 OFF
-		delayMs(1000);							// Loop delay
+		delayMs(1000);				// Loop delay
 	}
 }
 
