d0a34d761235a912ceaab2c29c8c745042279d46
Divyam Soni <divyam.8116@gmail.com>
2021-05-11 19:42:22 +0530

    updated GPIO_Input

--- Bare-metal/02-GPIO_Input/main.c
+++ Bare-metal/02-GPIO_Input/main.c
@@ -1,27 +1,33 @@
+/*****************************
+** Pressing user button B1 	**
+** turns on LD1, 2, 3		**
+** using polling			**
+** B1 <-> PC13				**
+** LD1 - PB0				**
+** LD2 - PB7				**
+** LD3 - PB14 				**
+******************************/
+
 #include "stm32f7xx.h"                  // Device header
-// B1 <-> PC13
-// LD1 - PB0
-// LD2 - PB7
-// LD3 - PB14
 
 int main(){
-		
+
 		RCC->AHB1ENR |=  0x2; 			//Enables clock access to GPIOB
 		RCC->AHB1ENR |=  0x4; 			//Enables clock access to GPIOC
-			
+
 		// Setting Output mode for LEDs
-	
-		GPIOB->MODER |= 0x1;			// LD1 <-> PB0	
+
+		GPIOB->MODER |= 0x1;			// LD1 <-> PB0
 		GPIOB->MODER |= 0x4000;			// LD2 <-> PB7
-		GPIOB->MODER |= 0x010000000;		// LD3 <-> PB14
-		
+		GPIOB->MODER |= 0x010000000;	// LD3 <-> PB14
+
 		while(1){
-			if(GPIOC->IDR & 0x2000){ 
-			// if PC13 is high 	
-				GPIOB->ODR = 0x4081;  	// LED 1,2,3 ON!	
+			if(GPIOC->IDR & 0x2000){
+			// if PC13 is high
+				GPIOB->ODR = 0x4081UL;  	// LED 1,2,3 ON!
 			}
 			else{
-				GPIOB->ODR = 0;		// LED 1,2,3 OFF				
+				GPIOB->ODR &=~ 0x4081UL;	// LED 1,2,3 OFF
 			}
 		}
 }
