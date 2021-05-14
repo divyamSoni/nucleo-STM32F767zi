833d17d00673038383056b08adc9cdc404ca20df
Divyam Soni <divyam.8116@gmail.com>
2021-05-14 17:23:23 +0530

    updated Systic_Basic

--- Bare-metal/03-Systic_Basic/main.c
+++ Bare-metal/03-Systic_Basic/main.c
@@ -1,22 +1,24 @@
-/*
+/*****************************
+** Use SysTick to generate 	**
+** constant delay to blink 	**
+** an LED					**
+*****************************/
 
-*/
-
-#include "stm32f7xx.h"                  	// Device header
+#include "stm32f7xx.h"        // Device header
 
 int main(void){
-	RCC->AHB1ENR |= 1;			// Enable GPIOA
-	GPIOA->MODER |= 0x400;								
-	
+	RCC->AHB1ENR |= 2;		   // Enable clock access to GPIOB
+	GPIOB->MODER |= 0x4000;	   // GPIOB_PIN_14 output mode
+
 	//Configure Systick
-	SysTick->LOAD = 3200000 - 1; 		// 200ms (@16MHz)
-	SysTick->VAL = 0;
-	SysTick->CTRL = 5;
-	
+	SysTick->LOAD = 3200000 - 1;   // 200ms (@16MHz)
+	SysTick->VAL = 0;              // Clear current value register
+	SysTick->CTRL = 5;             // Enable SysTick
+
 	while(1){
 		// Check if count flag is set
 		if(SysTick->CTRL & 0x10000){
-			GPIOA->ODR ^= 0x20; 	// Toggle LED
+			GPIOB->ODR ^= 0x80; 	// Toggle LED2 - BLUE
 		}
 	}
 }
